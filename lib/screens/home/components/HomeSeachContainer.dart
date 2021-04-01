5491c127dccf4b98c18c34e9e6caa5cf782bb242
riyad mac2 <riyad@kalke.co>
2021-04-01 21:36:36 +0600

    popup suggestion md

--- lib/screens/home/components/HomeSeachContainer.dart
+++ lib/screens/home/components/HomeSeachContainer.dart
@@ -1,19 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:surfer/constants.dart';
 
-class HomeSearchContainer extends StatefulWidget {
-  @override
-  _HomeSearchContainerState createState() => _HomeSearchContainerState();
-}
-
-class _HomeSearchContainerState extends State<HomeSearchContainer> {
-  String filtervalue = "One";
-
-  String changeFilter(value) {
-    filtervalue = value;
-    setState(() {});
-    return filtervalue;
-  }
+class HomeSearchContainer extends StatelessWidget {
+  const HomeSearchContainer({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -30,58 +21,13 @@ class _HomeSearchContainerState extends State<HomeSearchContainer> {
               ),
               hintText: "Search",
               hintStyle: searchtext,
-              suffixText: filtervalue,
-              suffixIcon: MyStatefulWidget(filtervalue, changeFilter),
+              suffixText: "Filter",
+              suffixIcon: Icon(
+                Icons.filter_3_sharp,
+                color: Colors.black,
+              ),
               enabledBorder: InputBorder.none,
               focusedBorder: InputBorder.none),
         ));
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget(this.value, this.press);
-
-  final value;
-  final press;
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  String value;
-  var press;
-  @override
-  void initState() {
-    super.initState();
-    value = widget.value;
-    press = widget.press;
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: value,
-      icon: const Icon(Icons.arrow_downward),
-      iconSize: 24,
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (newValue) {
-        value = press(newValue);
-        setState(() {});
-      },
-      items: <String>['One', 'Two', 'Free', 'Four']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
   }
 }
